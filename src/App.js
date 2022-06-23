53de8df3f4fbecb344189b5e9ec83216b24598f3
bunty343 <107154389+bunty343@users.noreply.github.com>
2022-06-23 16:00:44 -0700

    june22

--- src/App.js
+++ src/App.js
@@ -2,7 +2,8 @@ import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import LinkContainer from "./components/LinkContainer"
-
+import Accordion from './components/pages/Accordion';
+import Link from './components/Link';
 function App() {
   const [hideLink, setHideLink] = useState(false)
   const hideLinkHandler = () => {
