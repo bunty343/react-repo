882393d0b68894cab3b4f47bce2ac44eb3b620ea
bunty343 <107154389+bunty343@users.noreply.github.com>
2022-06-30 16:26:54 -0700

    june28-29

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import './App.css';
-import Counter from './components/counter/Counter';
-import Accordion from "./components/pages/Accordion/index"
-import User from './components/user/User';
 import CreateAccount from './components/auth/CreateAccount';
+// import Counter from './components/counter/Counter';
+// import Accordion from "./components/pages/Accordion/index"
+// import User from './components/user/User';
+
 function App() {
   return (
     <div className="App">
-      <Accordion />
+      {/* <Accordion />
       <br />
       <Counter />
       <br />
       <User />
+      <br /> */}
+      <CreateAccount />
     </div>
   )
 }
 
 export default App;
+
+
 
 
